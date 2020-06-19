ab59729b576b6613b9cbc08d2bc55e314cc62105
Victor Cesar Lopez <civ187@gmail.com>
2020-06-19 16:49:40 -0500

    added current date to top of page

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+/* var text = $(this).siblings(".description").val();
+var time = $(this).parent().attr("id"); */
+
+});
+
+
+/* function hourTracker(){
+var currentHour = moment().hour();   
+} */

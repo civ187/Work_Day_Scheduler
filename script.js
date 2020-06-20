d8cc9b9617fabbf4bebe24c180ae21c3bc4a34c5
Victor Cesar Lopez <civ187@gmail.com>
2020-06-20 10:45:23 -0500

    updated javascript

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-$(document).ready(function () {
+$(document).ready(function () {   
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-/* var text = $(this).siblings(".description").val();
-var time = $(this).parent().attr("id"); */
+        $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-});
+        localStorage.setItem(time, text);
+    })
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour18 .description").val(localStorage.getItem("hour18"));
+$("#hour19 .description").val(localStorage.getItem("hour19"));
 
-
-/* function hourTracker(){
+function hourTracker(){
 var currentHour = moment().hour();   
-} */
+}
+hourTracker();
+})

5548871e55834750e3d279be0cfeb30c0e880495
Victor Cesar Lopez <civ187@gmail.com>
2020-06-20 13:35:11 -0500

    Final commit

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
 $(document).ready(function () {   
+    
+    //displays current date
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    
+    //save button and click listener to save user input
+    $(".saveBtn").on("click", function () {
+        console.log(this); 
+        var task = $(this).siblings(".description").val();
+        var hour = $(this).parent().attr("id");
 
-        $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        localStorage.setItem(time, text);
+        //sets hours and task into local storage
+        localStorage.setItem(hour, task);
     })
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
-$("#hour18 .description").val(localStorage.getItem("hour18"));
-$("#hour19 .description").val(localStorage.getItem("hour19"));
+    
+    //loads any saved data from local Storage.
+    $("#hour08 .description").val(localStorage.getItem("hour08"));
+    $("#hour09 .description").val(localStorage.getItem("hour09"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour19 .description").val(localStorage.getItem("hour19"));
 
-function hourTracker(){
-var currentHour = moment().hour();   
-}
-hourTracker();
+    function hourTracker(){
+
+        //retrieves current hour
+        var currentHour = moment().hour();
+        
+        //loop function to color code the hours as past, present, future.
+        $(".time-block").each(function() {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+            
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+            else if (blockHour === currentHour){
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker();
 })
